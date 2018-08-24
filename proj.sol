6a9118b2d617b3c3f5e6dc38e36c7728255a3085
nurgalive <azat.nur@gmail.com>
2018-08-24 23:54:00 +0200

    Added function modifiers

--- proj.sol
+++ proj.sol
@@ -2,22 +2,31 @@ pragma solidity ^0.4.24;
 
 contract Coursetro {
     
-   string fName;
-   uint age;
+    string fName;
+    uint age;
+    address
+
+    function Coursetro() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOnwer {
+        require(msg.sender == owner);
+        _;
+    }
    
-   event Instructor(
-       string name, 
-       uint age
-       );
+    event Instructor(
+        string name, 
+        uint age);
+    
+    function setInstructor(string _fName, uint _age) onlyOwner public {
+        fName = _fName;
+        age = _age;
+        Instructor(_fName, _age);
+    }
    
-   function setInstructor(string _fName, uint _age) public {
-       fName = _fName;
-       age = _age;
-       Instructor(_fName, _age);
-   }
-   
-   function getInstructor() public constant returns (string, uint) {
-       return (fName, age);
-   }
+    function getInstructor() public constant returns (string, uint) {
+        return (fName, age);
+    }
     
 }
